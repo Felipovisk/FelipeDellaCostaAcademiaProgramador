c49dc735adad01acd309198806726f3b5082111e
Felipovisk <felipe.dellacosta@gmail.com>
2014-08-06 09:09:49 -0300

    finalizado exercicios.

--- AcademiaProgramador/Unidade2/Orientacao_a_Objetos/TestaCartaoDeCredito.cs
+++ AcademiaProgramador/Unidade2/Orientacao_a_Objetos/TestaCartaoDeCredito.cs
@@ -14,15 +14,25 @@ namespace Unidade2.Orientacao_a_Objetos
             cdc1.numero = 19736576;
             cdc1.dataDeValidade = "08/2018";
 
+            Cliente c1 = new Cliente();
+            c1.codigo = 198733;
+            c1.nome = "Saudosista";
+
+
             CartaoDeCredito cdc2 = new CartaoDeCredito();
             cdc2.numero = 987657319;
             cdc2.dataDeValidade = "10/2021";
+
+            cdc1.cliente = c1;
 
             Console.WriteLine(cdc1.numero);
             Console.WriteLine(cdc1.dataDeValidade);
 
             Console.WriteLine(cdc2.numero);
             Console.WriteLine(cdc2.dataDeValidade);
+
+            Console.WriteLine(c1.codigo);
+            Console.WriteLine(c1.nome);
 
             Console.ReadKey();
 
